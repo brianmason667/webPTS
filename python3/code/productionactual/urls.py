1f87588f6bb1ac88420280f96d3b9dbe651260fa
brian mason <brianmason667@gmail.com>
2021-02-16 03:15:03 -0600

    added a way to open a production actual

--- python3/code/productionactual/urls.py
+++ python3/code/productionactual/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.BlankProductionActualView, name='BlankProductionActual'),
     path('new/', views.NewProductionActualView, name='NewProductionActual'),
+    path('open/', views.OpenProductionActualView.as_view(), name='OpenProductionActual'),
     path('<uuid:pk>/', views.ProductionActualView, name="ProductionActual")
 ]
 
--- python3/code/productionactual/views.py
+++ python3/code/productionactual/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .forms import *
 from .models import *
 
@@ -38,7 +39,21 @@ def NewProductionActualView(request):
         context['form']= form
         return render(request, "newproductionactual.html", context)
 
-## /ProductionActual/20a0904a-ba5f-4a67-a163-03110dae00ce/ ##
+## /ProductionActual/open ##
+# def OpenProductionActualView(request):
+#     return render(request, "openproductionactual.html")
+class OpenProductionActualView(generic.ListView):
+    template_name = 'openproductionactual.html'
+    context_object_name = 'latest_pa_list'
+
+    def get_queryset(self):
+        """
+        Return the last five created productionactuals (not including those set to be
+        created in the future).
+        """
+        return ProductionActual.objects.filter(pa_date__lte=timezone.now()).order_by('-pa_date')[:50]
+
+## /ProductionActual/20a0904a-ba5f-4a67-a163-03110dae00ce/ ## ex: an opened production actual
 def ProductionActualView(request, pk):
     Production_Actual = get_object_or_404(ProductionActual, pk=pk)
     context = {
