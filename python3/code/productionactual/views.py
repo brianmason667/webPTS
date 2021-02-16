511cdd375144bcc4ae0420fb464b04508a86bb44
brian mason <brianmason667@gmail.com>
2021-02-15 18:28:56 -0600

    new production actual form now closes and redirects when done, and it works

--- python3/code/productionactual/views.py
+++ python3/code/productionactual/views.py
@@ -11,36 +11,24 @@ def ProductionActualView(request):
     return render(request, "productionactual.html", )
 
 def NewProductionActual(request):
-    context ={}
-    form = NewProductionActualForm(request.POST or None)
-    if form.is_valid():
-        # not save until form.save()
-        form = form.save(commit=False)
-        # set user that is logged in to the form user
-        form.user = request.user    
-        form.save()
+    if request.method == 'POST':
+        form = NewProductionActualForm(request.POST)
+        if form.is_valid():
+            # not save until form.save()
+            form = form.save(commit=False)
+            # set user that is logged in to the form user
+            form.user = request.user    
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        context ={}
+        form = NewProductionActualForm()
+        if form.is_valid():
+            # not save until form.save()
+            form = form.save(commit=False)
+            # set user that is logged in to the form user
+            form.user = request.user    
+            form.save()
 
-    context['form']= form
-    return render(request, "newproductionactual.html", context)
-
-
-    # ProductionActual_ID = ProductionActual.ProductionActual_id
-    # ProductionActual_Instance = get_object_or_404(ProductionActual, pk=ProductionActual_ID)
-
-    # if request.method == 'POST':
-    #     form = NewProductionActualForm(request.POST)
-    #     if form.is_valid():
-    #         ProductionActual_Instance.pa_date = form.cleaned_data['pa_date']
-    #         ProductionActual_Instance.save()
-
-    #         return HttpResponseRedirect(reverse('index') )
-    # else:
-    #     proposed_pa_date = datetime.date.today()
-    #     form = NewProductionActualForm(initial={'pa_date': proposed_pa_date})
-
-    # context = {
-    #     'form': form,
-    #     'ProductionActual_Instance': ProductionActual_Instance,
-    # }
-
-    # return render(request, 'productionactual/newproductionactual.html', context)
+        context['form']= form
+        return render(request, "newproductionactual.html", context)
