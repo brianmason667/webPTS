991c48a1c4d9282949261e95bd3667d71c9ed9b7
brian mason <brianmason667@gmail.com>
2021-02-28 22:45:52 -0600

    added losttime url

--- python3/code/productionactual/urls.py
+++ python3/code/productionactual/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.BlankProductionActualView, name='BlankProductionActual'),
-    path('new/', views.NewProductionActualView, name='NewProductionActual'),
-    path('open/', views.OpenProductionActualView.as_view(), name='OpenProductionActual'),
+    path('New/', views.NewProductionActualView, name='NewProductionActual'),
+    path('Open/', views.OpenProductionActualView.as_view(), name='OpenProductionActual'),
     path('<uuid:pk>/', views.ProductionActualView, name="ProductionActual"),
-    path('newproduct/', views.NewProductView, name='NewProduct'),
+    path('<uuid:pk>/LostTime', views.LostTimeView, name="LostTime"),
+    path('NewProduct/', views.NewProductView, name='NewProduct'),
 ]
 
 
