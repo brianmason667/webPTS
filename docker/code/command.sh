4b11574ed8152edb59609ff3c1b2e5a77c02b65c
brian mason <brianmason667@gmail.com>
2021-03-29 02:42:00 -0500

    trying to fix socket problem

--- docker/code/command.sh
+++ docker/code/command.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-daphne -u /tmp/daphne.sock webpts3.asgi:application &
+# rm /tmp/daphne.sock &
+# sleep 5s 
+daphne  -u /tmp/daphne.sock webpts3.asgi:application &
+sleep 5s
 httpd-foreground
